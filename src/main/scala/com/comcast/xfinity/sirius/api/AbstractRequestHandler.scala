ac2c728ce0587013e7b7bb71d25aeee07fc7f1c0
Trevor Lalish-Menagh <trev@trevreport.org>
2012-05-10 11:46:09 -0400

    made abstract request handler...abstract
    Change-Id: I437b298800695c79f30d1c6456acab1fcf8f8325

--- src/main/scala/com/comcast/xfinity/sirius/api/AbstractRequestHandler.scala
+++ src/main/scala/com/comcast/xfinity/sirius/api/AbstractRequestHandler.scala
@@ -1,6 +1,6 @@
 package com.comcast.xfinity.sirius.api
 
-trait AbstractRequestHandler extends RequestHandler {
+abstract class AbstractRequestHandler extends RequestHandler {
     def handle(method: RequestMethod, key: String, body: Array[Byte]): Array[Byte] = method match {
       case RequestMethod.PUT => doPut(key, body)
       case RequestMethod.GET => doGet(key, body)
