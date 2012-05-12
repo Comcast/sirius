8fc62b24e6f36fe64160b3323c5cbb1686034f38
ktomasette <kristofer_tomasette@comcast.com>
2012-05-11 23:04:22 -0400

    Refactor WriteAheadLog in preparation for actually writing to a file * rename to WriteAheadLogEntry ... since this class's responsibility is to pack and unpack a log entry * make it mixin the MD5Checksum and Base64PayloadCodec ... an attempt to separate concerns * add some tests to exercise serializing and deserializing
    Change-Id: Ie65d9b5eb7c40f8abe8d1bd5ec6ab41b3eca75be

--- src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/WriteAheadLogEntry.scala
+++ src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/WriteAheadLogEntry.scala
@@ -1,31 +1,29 @@
 package com.comcast.xfinity.sirius.writeaheadlog
 
-import java.security.MessageDigest
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 import scala.collection.mutable.StringBuilder
-import org.apache.commons.codec.binary.Base64
-import org.joda.time.format.DateTimeFormatter
-import org.joda.time.format.ISODateTimeFormat
 import org.joda.time.DateTimeZone
+import org.joda.time.format.{DateTimeFormatter, ISODateTimeFormat}
 
 case class LogData(actionType: String, key: String, sequence: Long, timestamp: Long, payload: Array[Byte])
 
 /**
  * Responsible for creating entries in the Sirius write ahead log.
  */
-class WriteAheadLog {
-  val dateTimeFormatter = ISODateTimeFormat.basicDateTime()
-  val md5Digest = MessageDigest.getInstance("MD5");
-  val whitespacePattern = Pattern.compile("\\s")
-  val pipePattern = Pattern.compile("\\|")
-  
+class WriteAheadLogEntry extends LogEntry with MD5Checksum with Base64PayloadCodec {
+  val dateTimeFormatter: DateTimeFormatter = ISODateTimeFormat.basicDateTime()
+  val whitespacePattern: Pattern = Pattern.compile("\\s")
+  val pipePattern: Pattern = Pattern.compile("\\|")
+
+  private[writeaheadlog] var logEntry: LogData = _
+
   /**
    * Creates a single log entry for the write ahead log.
    */
-  def createLogEntry(entryData: LogData): String = {
-    entryData match {
+  def serialize(): String = {
+    logEntry match {
       case LogData(actionType, key, sequence, timestamp, payload) =>
+
         validateKey(key)
 
         val entryBuilder = new StringBuilder()
@@ -37,36 +35,36 @@ class WriteAheadLog {
         entryBuilder.append("|")
         entryBuilder.append(dateTimeFormatter.withZone(DateTimeZone.UTC).print(timestamp))
         entryBuilder.append("|")
-        entryBuilder.append(Base64.encodeBase64String(payload))
+        entryBuilder.append(encodePayload(payload))
         entryBuilder.append("|")
-        val bytesSoFar = entryBuilder.toString().getBytes("US-ASCII")
-        entryBuilder.append(Base64.encodeBase64String(md5Digest.digest(bytesSoFar)))
+        entryBuilder.append(generateChecksum(entryBuilder.toString()))
         entryBuilder.append("\r")
-
         entryBuilder.toString()
+      case _ => throw new IllegalStateException("No data to serialize. Must deserialize something first.")
     }
   }
-  
+
+
   /**
    * Read a single log entry from a String.
    */
-  def parseLogEntry(logEntry : String): LogData = {
-    val Array(actionType, key, sequence, timestamp, payload, checksum) = logEntry.split("\\|")
-    
-    
-    return LogData(actionType, key, sequence.toLong,dateTimeFormatter.parseDateTime(timestamp).getMillis(), Base64.decodeBase64(payload))
+  def deserialize(rawData: String) {
+    val Array(actionType, key, sequence, timestamp, payload, checksum) = rawData.split("\\|")
+    logEntry = LogData(actionType, key, sequence.toLong, dateTimeFormatter.parseDateTime(timestamp).getMillis(), decodePayload(payload))
   }
-  
+
   def validateKey(key: String) = {
     val whitespaceMatcher = whitespacePattern.matcher(key)
     val hasWhitespace = whitespaceMatcher.find()
     val pipeMatcher = pipePattern.matcher(key)
     val hasPipe = pipeMatcher.find()
-    
-    if(hasPipe)
+
+    if (hasPipe) {
       throw new IllegalStateException("Key can't have | in it.")
-    if(hasWhitespace)
+    }
+    if (hasWhitespace) {
       throw new IllegalStateException("Key can't have whitespace in it.")
-    
+    }
+
   }
 }
---
+++ src/test/scala/com/comcast/xfinity/sirius/writeaheadlog/WriteAheadLogEntryTest.scala
@@ -0,0 +1,92 @@
+package com.comcast.xfinity.sirius.writeaheadlog
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.BeforeAndAfter
+import org.scalatest.FunSpec
+import java.security.MessageDigest
+import org.apache.commons.codec.binary.Base64
+import org.mockito.Mockito._
+
+@RunWith(classOf[JUnitRunner])
+class WriteAheadLogEntryTest extends FunSpec with BeforeAndAfter {
+  var logEntry: WriteAheadLogEntry = _
+  var mockCodec: Base64 = _
+  var mockMessageDigest: MessageDigest = _
+
+  before {
+    logEntry = new WriteAheadLogEntry()
+
+
+  }
+
+
+
+  describe("A Sirius write ahead log entry") {
+    it("should serialize and deserialize to the same thing") {
+      val rawLogEntry = "PUT|key|123|19700101T000012.345Z|QQ==|Q6UvDVS1BZtbrPcdyUwakQ==\r"
+      logEntry.deserialize(rawLogEntry)
+      assert(rawLogEntry === logEntry.serialize())
+    }
+
+    it("should throw exception if never was deserialized") {
+      intercept[IllegalStateException] {
+        logEntry.serialize()
+      }
+
+    }
+
+    it("should serialize to a string representation of the log contents.") {
+      val logData = new LogData("PUT", "key", 123L, 12345L, Array[Byte](65))
+      logEntry.logEntry = logData;
+      val expectedLogEntry = "PUT|key|123|19700101T000012.345Z|QQ==|Q6UvDVS1BZtbrPcdyUwakQ==\r"
+      assertLogEntriesEqual(expectedLogEntry, logEntry.serialize())
+    }
+
+    it("should properly encodes payloads that have a | character when serializing.") {
+      val logData = new LogData("PUT", "key", 123L, 12345L, Array[Byte](65, 124, 65))
+      logEntry.logEntry = logData;
+      val expectedLogEntry = "PUT|key|123|19700101T000012.345Z|QXxB|Uw81FQiQ3WGGglvYtWG0ew==\r"
+      assertLogEntriesEqual(expectedLogEntry, logEntry.serialize())
+    }
+
+    it("should throw an exception when attempting to use a key with a | character when serializing.") {
+      val logData = new LogData("PUT", "key|foo|bar", 123L, 12345L, Array[Byte](65))
+      logEntry.logEntry = logData
+      intercept[IllegalStateException] {
+        logEntry.serialize()
+      }
+    }
+
+    // TODO: Figure out the right way to test that we're not allowing any whitespace
+    // using FunSpec w/o a bunch of cut and paste.
+    it("should throw an exception when attempting to use a key with Unicode U+0020 in it, when serializing.") {
+      val logData = new LogData("PUT", "key foo bar", 123L, 12345L, Array[Byte](65))
+      logEntry.logEntry = logData
+      intercept[IllegalStateException] {
+        logEntry.serialize()
+      }
+    }
+
+    it("should throw an exception when attempting to use a key with Unicode U+000A in it when serializing.") {
+      val logData = new LogData("PUT", "key\nfoo\nbar", 123L, 12345L, Array[Byte](65))
+      logEntry.logEntry = logData
+      intercept[IllegalStateException] {
+        logEntry.serialize()
+      }
+    }
+
+  }
+
+  def assertLogEntriesEqual(expected: String, underTest: String) = {
+    val Array(expectedActionType, expectedKey, expectedSequence, expectedTimestamp, expectedPayload, expectedChecksum) = expected.split("\\|")
+    val Array(testActionType, testKey, testSequence, testTimestamp, testPayload, testChecksum) = underTest.split("\\|")
+
+    assert(testActionType === expectedActionType)
+    assert(testKey === expectedKey)
+    assert(testSequence === expectedSequence)
+    assert(testTimestamp === expectedTimestamp)
+    assert(testPayload === expectedPayload)
+    assert(testChecksum == expectedChecksum)
+  }
+}
