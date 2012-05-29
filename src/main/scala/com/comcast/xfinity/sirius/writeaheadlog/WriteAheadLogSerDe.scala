0069a00941318fe4e4dca3a17fb8a932a6fa98a4
ktomasette <kristofer_tomasette@comcast.com>
2012-05-29 13:57:17 -0400

    add logging to WriteAheadLogSerDe
    Change-Id: I2b1e22c58a80597831f454ad6bf7747cddc03329

--- src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/WriteAheadLogSerDe.scala
+++ src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/WriteAheadLogSerDe.scala
@@ -2,11 +2,13 @@ package com.comcast.xfinity.sirius.writeaheadlog
 
 import java.util.regex.Pattern
 import org.joda.time.format.ISODateTimeFormat
+import org.slf4j.LoggerFactory
 
 /**
  * Responsible for creating entries in the Sirius write ahead log.
  */
 class WriteAheadLogSerDe extends LogDataSerDe with Checksum with Base64PayloadCodec {
+  private val logger = LoggerFactory.getLogger(classOf[WriteAheadLogSerDe])
   val whitespacePattern: Pattern = Pattern.compile("\\s")
   val pipePattern: Pattern = Pattern.compile("\\|")
 
@@ -28,10 +30,13 @@ class WriteAheadLogSerDe extends LogDataSerDe with Checksum with Base64PayloadCo
   /**
    * Retrieve a log entry from the write ahead log
    */
-  def deserialize(rawData: String): LogData = 
+  def deserialize(rawData: String): LogData = {
+    logger.debug("Raw Data: {}", rawData)
+    logger.debug("data: {} -- {}", rawData.substring(24), rawData.substring(0, 24))
     validateAndDeserialize(rawData.substring(24), rawData.substring(0, 24))
+  }
 
-  
+
   private def checksummedLogEntry(base: String) =
     "%s%s".format(generateChecksum(base), base)
 
@@ -46,18 +51,18 @@ class WriteAheadLogSerDe extends LogDataSerDe with Checksum with Base64PayloadCo
     else
       throw new IllegalStateException("Key contains illegal character")
   }
-  
+
   private def validateAndDeserialize(data: String, checksum: String) = {
     if (validateChecksum(data, checksum))
       logDataOfString(data)
-    else 
+    else
       throw new SiriusChecksumException("Checksum does not match.")
   }
-  
+
   private def logDataOfString(logDataString: String) = logDataString.split("\\|") match {
     case Array("", action, key, seq, ts, payload) =>
       LogData(action, key, seq.toLong, parseTimestamp(ts), decodePayload(payload))
   }
 
-  def isKeyValid(key: String) = key.forall(!_.isWhitespace) && key.forall(_ != '|') 
+  def isKeyValid(key: String) = key.forall(!_.isWhitespace) && key.forall(_ != '|')
 }
