0acac849712422b7b7f3dfda2773ce59d06e6ce5
ktomasette <kristofer_tomasette@comcast.com>
2012-05-30 18:16:25 -0400

    push message handling for supervised actors into the supervisor.  This is to prevent a message being sent to an actor before it is created.  Haven't figured out how to test the supervisor yet... will be in a later commit
    Change-Id: I9c438039af647f83b8edbc684de95635d6181a6f

--- src/test/scala/com/comcast/xfinity/sirius/api/impl/SiriusImplTest.scala
+++ src/test/scala/com/comcast/xfinity/sirius/api/impl/SiriusImplTest.scala
@@ -4,10 +4,6 @@ import org.mockito.Matchers.any
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.spy
 import com.comcast.xfinity.sirius.api.RequestHandler
-import akka.actor.ActorRef
-import akka.actor.ActorSystem
-import akka.actor.Props
-import akka.actor.actorRef2Scala
 import akka.dispatch.Await
 import akka.testkit.TestProbe
 import akka.util.Timeout.durationToTimeout
@@ -17,46 +13,43 @@ import com.typesafe.config.ConfigFactory
 import akka.testkit.TestActor
 import org.mockito.Matchers
 import com.comcast.xfinity.sirius.NiceTest
+import akka.actor._
+import com.comcast.xfinity.sirius.writeaheadlog.LogWriter
 
 class SiriusImplTest extends NiceTest {
 
   var mockRequestHandler: RequestHandler = _
-  var stateActorProbe: TestProbe = _
-  var persistenceActorProbe: TestProbe = _
-  var paxosActorProbe: TestProbe = _
+
+
+  var supervisorActorProbe: TestProbe = _
   var underTest: SiriusImpl = _
   var spiedAkkaSystem: ActorSystem = _
   val timeout: Timeout = (5 seconds)
+  var logWriter: LogWriter = _
+
 
   before {
     spiedAkkaSystem = spy(ActorSystem("testsystem", ConfigFactory.parseString("""
     akka.event-handlers = ["akka.testkit.TestEventListener"]
     """)))
 
-    mockRequestHandler = mock[RequestHandler]
-    stateActorProbe = TestProbe()(spiedAkkaSystem)
-    stateActorProbe.setAutoPilot(new TestActor.AutoPilot {
+
+
+    supervisorActorProbe = TestProbe()(spiedAkkaSystem)
+    supervisorActorProbe.setAutoPilot(new TestActor.AutoPilot {
       def run(sender: ActorRef, msg: Any): Option[TestActor.AutoPilot] = msg match {
-          case Get(_) => sender ! "Got it".getBytes(); Some(this)
-      }
-    })
-    persistenceActorProbe = TestProbe()(spiedAkkaSystem)
-    paxosActorProbe = TestProbe()(spiedAkkaSystem)
-    paxosActorProbe.setAutoPilot(new TestActor.AutoPilot {
-      def run(sender: ActorRef, msg: Any): Option[TestActor.AutoPilot] = msg match {
+        case Get(_) => sender ! "Got it".getBytes(); Some(this)
         case Delete(_) => sender ! "Delete it".getBytes(); Some(this)
         case Put(_, _) => sender ! "Put it".getBytes(); Some(this)
       }
     })
 
-    doReturn(stateActorProbe.ref).when(spiedAkkaSystem).
-            actorFor(Matchers.eq("user/sirius/state"))
-    doReturn(persistenceActorProbe.ref).when(spiedAkkaSystem).
-            actorFor(Matchers.eq("user/sirius/persistence"))
-    doReturn(paxosActorProbe.ref).when(spiedAkkaSystem).
-            actorFor(Matchers.eq("user/sirius/paxos"))
-            
-    underTest = new SiriusImpl(mockRequestHandler, spiedAkkaSystem)
+    logWriter = mock[LogWriter]
+
+
+
+    underTest = new SiriusImpl(mockRequestHandler, spiedAkkaSystem, logWriter)
+    underTest.supervisor = supervisorActorProbe.ref
   }
 
   after {
@@ -64,23 +57,23 @@ class SiriusImplTest extends NiceTest {
   }
 
   describe("a SiriusScalaImpl") {
-    it("should send a Get message to the state actor when enqueueGet is called") {
+    it("should send a Get message to the supervisor actor when enqueueGet is called") {
       val key = "hello"
       assert("Got it".getBytes() === Await.result(underTest.enqueueGet(key), timeout.duration).asInstanceOf[Array[Byte]])
-      stateActorProbe.expectMsg(Get(key))
+      supervisorActorProbe.expectMsg(Get(key))
     }
-    
-    it("should send a Put message to the persistence actor when enqueuePut is called and get some \"ACK\" back") {
+
+    it("should send a Put message to the supervisor actor when enqueuePut is called and get some \"ACK\" back") {
       val key = "hello"
       val body = "there".getBytes()
       assert("Put it".getBytes() === Await.result(underTest.enqueuePut(key, body), timeout.duration).asInstanceOf[Array[Byte]])
-      paxosActorProbe.expectMsg(Put(key, body))
+      supervisorActorProbe.expectMsg(Put(key, body))
     }
 
-    it("should send a Delete message to the persistence worker when enqueueDelete is called and get some \"ACK\" back") {
+    it("should send a Delete message to the supervisor actor when enqueueDelete is called and get some \"ACK\" back") {
       val key = "hello"
       assert("Delete it".getBytes() === Await.result(underTest.enqueueDelete(key), timeout.duration).asInstanceOf[Array[Byte]])
-      paxosActorProbe.expectMsg(Delete(key))
+      supervisorActorProbe.expectMsg(Delete(key))
     }
   }
 }
