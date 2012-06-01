a7c11cbeeb9c8eb0414c02f433af7a45ed71315c
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-06-01 13:36:25 -0400

    Sirius standalone build support
    Copied mostly from octave, where it was quite useful.
    
    A build script to assemble a tarball with sirius and dependencies,
    as well as a launcher script that will appropriately configure the
    classpath and launch a scala shell to interact with sirius in.
    
    This will be useful for testing and demoing.
    
    Change-Id: Id0b0afd2c1b59c24b80b2665f0282a6f42beaf8d

---
+++ build-interactive.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+base=`dirname $0`
+base=`cd $base; pwd`
+
+DO_ARCHIVE=true
+if [ "x$1" == "--skip-tar" ]; then
+    DO_ARCHIVE=false
+    shift
+fi
+
+function run {
+    echo "$ $@"
+    "$@"
+    res=$?
+    if [ ! $res -eq 0 ]; then
+        echo 1>&2
+        echo "Failed with $res" 1>&2
+        echo 1>&2
+        exit $res
+    fi
+}
+
+if [ "$1" != "--skip-mvn" ]; then
+    run mvn clean package dependency:copy-dependencies 
+fi
+
+targetdir="$base/target"
+distdir="$base/target/sirius-standalone"
+
+if [ -d $distdir ]; then
+    run rm -rf "$distdir"
+fi
+
+run mkdir -p $distdir
+
+run mkdir $distdir/bin
+run cp $targetdir/sirius.jar $distdir/bin/
+run cp -p $base/src/main/bin/* $distdir/bin/
+
+run mkdir $distdir/lib
+run cp $targetdir/dependency/*.jar $distdir/lib
+
+echo
+echo "octave-dist available at $targetdir/sirius-standalone/"
+echo
+
+if [ "x$DO_ARCHIVE" == "xtrue" ]; then
+    run cd $targetdir
+    run tar -czf sirius-standalone.tar.gz sirius-standalone/
+    run cd -
+    echo
+    echo "sirius-standalone tarball available at $targetdir/sirius-standalone.tar.gz"
+    echo
+fi
+
---
+++ src/main/bin/bootstrap.scala
@@ -0,0 +1,15 @@
+import com.comcast.xfinity.sirius.admin._
+import com.comcast.xfinity.sirius.api._
+import com.comcast.xfinity.sirius.api.impl._
+import com.comcast.xfinity.sirius.api.impl.paxos._
+import com.comcast.xfinity.sirius.api.impl.persistence._
+import com.comcast.xfinity.sirius.api.impl.state._
+import com.comcast.xfinity.sirius.writeaheadlog._
+
+import akka.actor._
+
+println()
+println("Most of the Sirius classes have been imported, nonetheless we may have missed some...")
+println("As a convenience, some akka classes have been imported as well")
+println()
+
