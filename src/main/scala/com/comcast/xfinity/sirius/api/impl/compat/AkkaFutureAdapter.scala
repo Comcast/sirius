e8d5a65eb4c8ad191c2bda5fa7ea9d9d518103e4
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-07-05 15:30:29 -0400

    Scaladoc on AkkaFutureAdapter
    Change-Id: Ice910ac65274cdef9ece2431813f25f2623c27c5

--- src/main/scala/com/comcast/xfinity/sirius/api/impl/compat/AkkaFutureAdapter.scala
+++ src/main/scala/com/comcast/xfinity/sirius/api/impl/compat/AkkaFutureAdapter.scala
@@ -4,15 +4,35 @@ import akka.dispatch.{Await, Future => AkkaFuture}
 import akka.util.duration._
 import java.util.concurrent.{TimeoutException, ExecutionException, TimeUnit, Future}
 
+/**
+ * Class encapsulating a {@link akka.dispatch.Future} in a
+ * {@link java.util.concurrent.Future}
+ *
+ * @param akkaFuture the {@link akka.dispatch.Future} to wrap
+ */
 class AkkaFutureAdapter[T](akkaFuture: AkkaFuture[T]) extends Future[T] {
 
+  /**
+   * Not implemented, you may not cancel an Akka Future
+   */
   def cancel(mayInterrupt: Boolean): Boolean =
     throw new IllegalStateException("Not implemented")
 
+  /**
+   * Always returns true, since this is not cancellable
+   *
+   * @return true always
+   */
   def isCancelled: Boolean = false // if not cancellable can't be cancelled
 
+  /**
+   * {@inheritDoc}
+   */
   def isDone: Boolean = akkaFuture.isCompleted
 
+  /**
+   * {@inheritDoc}
+   */
   def get: T =
     try {
       // there is no way in hell this can time out
@@ -21,6 +41,9 @@ class AkkaFutureAdapter[T](akkaFuture: AkkaFuture[T]) extends Future[T] {
       case e => throw new ExecutionException(e)
     }
 
+  /**
+   * {@inheritDoc}
+   */
   @throws(classOf[ExecutionException])
   @throws(classOf[TimeoutException])
   def get(l: Long, timeUnit: TimeUnit) =
