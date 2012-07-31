579411ce74adf1c262013035edbdeeb12f2dd9b6
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-07-31 15:10:37 -0400

    SiriusStateActor wraps exceptions
    Also a little bit of test refactoring
    Now dried up
    
    Change-Id: Ibba71291914411183020587189ea54d97c590530

--- src/test/scala/com/comcast/xfinity/sirius/api/impl/state/SiriusStateActorTest.scala
+++ src/test/scala/com/comcast/xfinity/sirius/api/impl/state/SiriusStateActorTest.scala
@@ -1,77 +1,142 @@
 package com.comcast.xfinity.sirius.api.impl.state
 
-import org.mockito.Mockito.spy
-import org.mockito.Mockito.verify
 import org.mockito.Matchers
-import org.mockito.Mockito
-import com.comcast.xfinity.sirius.api.RequestHandler
-import com.typesafe.config.ConfigFactory
+import org.mockito.Mockito._
 import akka.actor.ActorSystem
-import akka.testkit.TestActorRef
-import akka.util.Timeout.durationToTimeout
-import akka.util.duration.intToDurationInt
-import akka.util.Timeout
 import com.comcast.xfinity.sirius.api.impl.{SiriusState, Delete, Get, Put}
 import com.comcast.xfinity.sirius.NiceTest
 import akka.agent.Agent
+import com.comcast.xfinity.sirius.api.{SiriusResult, RequestHandler}
+import akka.testkit.{TestProbe, TestActorRef}
+import org.scalatest.BeforeAndAfterAll
 
-class SiriusStateActorTest extends NiceTest {
+class SiriusStateActorTest extends NiceTest with BeforeAndAfterAll {
+
+  implicit val actorSystem = ActorSystem("SiriusStateActorTest")
 
   var mockRequestHandler: RequestHandler = _
   var testActor: TestActorRef[SiriusStateActor] = _
-  var underTest: SiriusStateActor = _
-  var spiedAkkaSystem: ActorSystem = _
   var mockSiriusStateAgent: Agent[SiriusState] = _
-  val timeout: Timeout = (5 seconds)
 
   before {
-    spiedAkkaSystem = spy(ActorSystem("testsystem", ConfigFactory.parseString("""
-    akka.event-handlers = ["akka.testkit.TestEventListener"]
-    """)))
-    
     mockRequestHandler = mock[RequestHandler]
     mockSiriusStateAgent = mock[Agent[SiriusState]]
 
-    testActor = TestActorRef(new SiriusStateActor(mockRequestHandler, mockSiriusStateAgent))(spiedAkkaSystem)
-    underTest = testActor.underlyingActor
-
+    testActor = TestActorRef(new SiriusStateActor(mockRequestHandler, mockSiriusStateAgent))
   }
 
-  after {
-    spiedAkkaSystem.shutdown()
+  override def afterAll {
+    actorSystem.shutdown()
   }
 
-  describe("a SiriusStateWorker") {
+  describe("A SiriusStateWorker") {
 
-    it("should send an initialized message to StateActor on preStart()") {
-      verify(mockSiriusStateAgent).send(Matchers.any(classOf[SiriusState => SiriusState]
-      ))
+    describe("on startup") {
+      it("should send an initialized message to StateActor on preStart()") {
+        verify(mockSiriusStateAgent).send(Matchers.any[SiriusState => SiriusState])
+      }
     }
 
-    it("should forward proper PUT message to handler") {
-      val key = "key"
-      val body = "value".getBytes()
-      testActor ! Put(key, body)
-      verify(mockRequestHandler).handlePut(Matchers.eq(key), Matchers.eq(body))
+    describe("in response to a Put message") {
+      it ("must forward the message to handler and reply with the result " +
+          "when it succeeds") {
+        val senderProbe = TestProbe()
+        val key = "key"
+        val body = "value".getBytes
+
+        // the following is a little delicate, Array[Byte] are Java jawns, so
+        //  we must use the same array for comparing
+        doReturn(SiriusResult.some("It's alive!")).when(mockRequestHandler).
+          handlePut(Matchers.eq(key), Matchers.same(body))
+
+        senderProbe.send(testActor, Put(key, body))
+
+        senderProbe.expectMsg(SiriusResult.some("It's alive!"))
+      }
+
+      it ("must forward the message to the handler and reply with a result " +
+          "wrapping the exception when such occurs") {
+        val senderProbe = TestProbe()
+        val key = "key"
+        val body = "value".getBytes
+
+        val theException = new RuntimeException("well this sucks")
+
+        doThrow(theException).when(mockRequestHandler).
+          handlePut(Matchers.eq(key), Matchers.same(body))
+
+        senderProbe.send(testActor, Put(key, body))
+
+        senderProbe.expectMsg(SiriusResult.error(theException))
+      }
     }
 
-    it("should forward proper GET message to handler") {
-      val key = "key"
-      testActor ! Get(key)
-      verify(mockRequestHandler).handleGet(Matchers.eq(key))
+    describe("in response to a Get message") {
+      it("must forward the message to handler and reply with the result when " +
+          "it succeeds") {
+        val senderProbe = TestProbe()
+        val key = "key"
+
+        doReturn(SiriusResult.some("It really works!")).when(mockRequestHandler).
+          handleGet(Matchers.eq(key))
+
+        senderProbe.send(testActor, Get(key))
+
+        senderProbe.expectMsg(SiriusResult.some("It really works!"))
+      }
+
+      it ("must forward the message to the handler and reply with a result " +
+          "wrapping the exception when such occurs") {
+        val senderProbe = TestProbe()
+        val key = "key"
+
+        val theException = new RuntimeException("well this sucks")
+
+        doThrow(theException).when(mockRequestHandler).
+          handleGet(Matchers.eq(key))
+
+        senderProbe.send(testActor, Get(key))
+
+        senderProbe.expectMsg(SiriusResult.error(theException))
+      }
     }
 
-    it("should forward proper DELETE message to handler") {
-      val key = "key"
-      testActor ! Delete(key)
-      verify(mockRequestHandler).handleDelete(Matchers.eq(key))
+    describe("in response to a Delete message") {
+      it ("should forward the message to handler and reply with the result when " +
+          "it succeeds") {
+        val senderProbe = TestProbe()
+        val key = "key"
+
+        doReturn(SiriusResult.some("I ate too much")).when(mockRequestHandler).
+          handleDelete(Matchers.eq(key))
+
+        senderProbe.send(testActor, Delete(key))
+
+        senderProbe.expectMsg(SiriusResult.some("I ate too much"))
+      }
+
+      it ("must forward the message to the handler and reply with a result " +
+          "wrapping the exception when such occurs") {
+        val senderProbe = TestProbe()
+        val key = "key"
+
+        val theException = new RuntimeException("well this sucks")
+
+        doThrow(theException).when(mockRequestHandler).
+          handleDelete(Matchers.eq(key))
+
+        senderProbe.send(testActor, Delete(key))
+
+        senderProbe.expectMsg(SiriusResult.error(theException))
+      }
     }
 
-    it("should not invoke handler when given invalid message id") {
-      testActor ! "derp"
-      verify(mockRequestHandler, Mockito.never()).handleDelete(Matchers.any(classOf[String]))
-      verify(mockRequestHandler, Mockito.never()).handleGet(Matchers.any(classOf[String]))
-      verify(mockRequestHandler, Mockito.never()).handlePut(Matchers.any(classOf[String]), Matchers.any(classOf[Array[Byte]]))
-   }
+    describe("in response to all other messages") {
+      it ("should do nothing") {
+        intercept[MatchError] {
+          testActor.underlyingActor.receive("derp")
+        }
+      }
+    }
   }
 }
