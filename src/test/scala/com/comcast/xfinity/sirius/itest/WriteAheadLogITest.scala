9d5188f0bade8f70bf052accf1de96c1ac035dcb
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-08-02 14:08:37 -0400

    Simplify WriteAheadLogITest
    Tests should be more deterministic and are more focused
    
    Change-Id: Ie5431f36ff8dec0999888b250a47e2f57190204c

--- src/test/scala/com/comcast/xfinity/sirius/itest/WriteAheadLogITest.scala
+++ src/test/scala/com/comcast/xfinity/sirius/itest/WriteAheadLogITest.scala
@@ -1,124 +1,69 @@
 package com.comcast.xfinity.sirius.itest
 
-import akka.actor.ActorSystem
 import org.junit.rules.TemporaryFolder
 import com.comcast.xfinity.sirius.writeaheadlog._
 
 import com.comcast.xfinity.sirius.api.impl._
-import java.util.concurrent.TimeUnit
 
-import scalax.file.Path
 import com.comcast.xfinity.sirius.{TimedTest, NiceTest}
 
+object WriteAheadLogITest {
+  def readEntries(siriusLog: SiriusLog) =
+    siriusLog.foldLeft(List[OrderedEvent]())(
+      (acc, event) => event :: acc
+    ).reverse
+}
 
-class WriteAheadLogITest extends NiceTest with AkkaConfig with TimedTest {
+class WriteAheadLogITest extends NiceTest {
 
-  var sirius: SiriusImpl = _
-
-  var actorSystem: ActorSystem = _
+  import WriteAheadLogITest._
 
   val tempFolder = new TemporaryFolder()
-  var logFilename: String = _
 
   var siriusLog: SiriusFileLog = _
-  var clusterConfigPath: Path = _
 
   before {
     tempFolder.create()
-    logFilename = tempFolder.newFile("sirius_wal.log").getAbsolutePath
 
-    val clusterConfigFileName = tempFolder.newFile("cluster.conf").getAbsolutePath
-    clusterConfigPath = Path.fromString(clusterConfigFileName)
-
-    actorSystem = ActorSystem.create("Sirius")
-
-    val logWriter: SiriusFileLog = new SiriusFileLog(logFilename, new WriteAheadLogSerDe())
-
-    sirius = new SiriusImpl(
-      new StringRequestHandler(),
-      logWriter,
-      clusterConfigPath
-    )(actorSystem)
-    assert(waitForTrue(sirius.isOnline, 5000, 500), "Sirius took too long to boot (>5s)")
-
-    siriusLog = new SiriusFileLog(logFilename, new WriteAheadLogSerDe())
+    val logFilename = tempFolder.newFile("sirius_wal.log").getAbsolutePath
+    siriusLog = SiriusFileLog(logFilename)
   }
 
   after {
-    actorSystem.shutdown()
     tempFolder.delete()
-    actorSystem.awaitTermination()
   }
-
-  /**
-   * Convenience method that returns a List[OrderedEvent] containing the contents of a Log
-   *
-   */
-  def readEntries(): List[OrderedEvent] =
-    siriusLog.foldLeft[List[OrderedEvent]](Nil)((a, c) => c :: a).reverse
 
   describe("a Sirius Write Ahead Log") {
     it("should have 1 entry after a PUT") {
-      sirius.enqueuePut("1", "some body".getBytes).get(1, TimeUnit.SECONDS)
+      val entries = List(
+        OrderedEvent(1, 1234, Put("1", "some body".getBytes))
+      )
 
-      val logEntries = readEntries()
+      entries.foreach(siriusLog.writeEntry(_))
 
-      assert(1 === logEntries.size)
-
-      val put = logEntries(0).request.asInstanceOf[Put]
-      assert("1" === put.key)
-      assert("some body" === new String(put.body))
+      assert(entries === readEntries(siriusLog))
     }
 
     it("should have 2 entries after 2 PUTs") {
-      sirius.enqueuePut("1", "some body".getBytes).get(1, TimeUnit.SECONDS)
-      sirius.enqueuePut("2", "some other body".getBytes).get(1, TimeUnit.SECONDS)
+      val entries = List(
+        OrderedEvent(1, 1234, Put("1", "some body".getBytes)),
+        OrderedEvent(2, 1235, Put("2", "some other body".getBytes))
+      )
 
-      val logEntries = readEntries()
+      entries.foreach(siriusLog.writeEntry(_))
 
-      assert(2 === logEntries.size)
-
-      val put1 = logEntries(0).request.asInstanceOf[Put]
-      assert("1" === put1.key)
-      assert("some body" === new String(put1.body))
-
-      val put2 = logEntries(1).request.asInstanceOf[Put]
-      assert("2" === put2.key)
-      assert("some other body" === new String(put2.body))
+      assert(entries === readEntries(siriusLog))
     }
 
     it("should have a PUT and a DELETE entry after a PUT and a DELETE") {
-      sirius.enqueuePut("1", "some body".getBytes).get(1, TimeUnit.SECONDS)
-      sirius.enqueueDelete("1").get(1, TimeUnit.SECONDS)
+      val entries = List(
+        OrderedEvent(1, 1234, Put("1", "some body".getBytes)),
+        OrderedEvent(2, 1236, Delete("1"))
+      )
 
-      val logEntries = readEntries()
+      entries.foreach(siriusLog.writeEntry(_))
 
-      assert(2 === logEntries.size)
-
-      val put = logEntries(0).request.asInstanceOf[Put]
-      assert("1" === put.key)
-      assert("some body" === new String(put.body))
-
-      val delete = logEntries(1).request.asInstanceOf[Delete]
-      assert("1" === delete.key)
-    }
-
-    it("should have 2 PUT entries after 2 PUTs and a GET") {
-      sirius.enqueuePut("1", "some body".getBytes).get(1, TimeUnit.SECONDS)
-      sirius.enqueuePut("2", "some other body".getBytes).get(1, TimeUnit.SECONDS)
-      sirius.enqueueGet("1").get(1, TimeUnit.SECONDS)
-
-      val logEntries = readEntries()
-
-      assert(2 === logEntries.size)
-
-      val put1 = logEntries(0).request.asInstanceOf[Put]
-      assert("1" === put1.key)
-      assert("some body" === new String(put1.body))
-
-      val put2 = logEntries(1).request.asInstanceOf[Put]
-      assert("2" === put2.key)
-      assert("some other body" === new String(put2.body))
+      assert(entries === readEntries(siriusLog))
     }
 
   }
