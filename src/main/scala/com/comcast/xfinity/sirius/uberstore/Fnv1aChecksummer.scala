74cfd1abf4edb6dbdbf0783bfa05640b82e87bc8
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-08-24 12:49:14 -0400

    Base traits for UberStore
    Checksummer and the basic impl- supplies checksumming
    OrderedEventCodec and associated impl- supplies encoding
    And tests
    
    Change-Id: I7d02497f88a9b7b383e4b1d177a1a14a566ac4ed
    Reviewed-on: https://source.dev.cim.comcast.net/gerrit/4127
    Reviewed-by: Joe <joseph_campbell@comcast.com>
    Tested-by: michajlo matijkiw <michajlo_matijkiw@comcast.com>

---
+++ src/main/scala/com/comcast/xfinity/sirius/uberstore/Fnv1aChecksummer.scala
@@ -0,0 +1,26 @@
+package com.comcast.xfinity.sirius.uberstore
+
+/**
+ * Trait supplying the FNV-1a checksum algorithm, as defined by
+ *
+ * http://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+ */
+trait Fnv1aChecksummer {
+  final val fnvOffsetBasis = -3750763034362895579L
+  final val fnvPrime = 1099511628211L
+
+  /**
+   * @inheritdoc
+   */
+  def checksum(bytes: Array[Byte]): Long = {
+    var hash = fnvOffsetBasis
+    var i = 0
+
+    while (i < bytes.length) {
+      hash = fnvPrime * (hash ^ (bytes(i) & 0x0ff))
+      i += 1
+    }
+
+    hash
+  }
+}
---
+++ src/test/scala/com/comcast/xfinity/sirius/uberstore/BinaryEventCodecTest.scala
@@ -0,0 +1,115 @@
+package com.comcast.xfinity.sirius.uberstore
+
+import com.comcast.xfinity.sirius.NiceTest
+import java.lang.Object
+import java.util.Arrays
+import com.comcast.xfinity.sirius.api.impl.{Delete, Put, OrderedEvent}
+import java.nio.{ByteOrder, ByteBuffer}
+
+class BinaryEventCodecTest extends NiceTest {
+
+  val underTest = new Object with BinaryEventCodec
+
+  describe("serialize") {
+    it ("must properly serialize Puts and be the inverse of deserialize") {
+      val key = "hello"
+      val body = "world".getBytes
+
+      val expected = ByteBuffer.allocate(8 + 8 + 4 + 4 + 4 + key.getBytes.length + body.length)
+
+      expected.order(ByteOrder.BIG_ENDIAN)
+      expected.putLong(1234)
+      expected.putLong(5678)
+      expected.putInt(1)
+      expected.putInt(key.getBytes.length)
+      expected.putInt(body.length)
+      expected.put(key.getBytes)
+      expected.put(body)
+
+      val orderedPut = OrderedEvent(1234, 5678, Put(key, body))
+      val serializedPut = underTest.serialize(orderedPut)
+      assert(Arrays.equals(expected.array, serializedPut),
+        "Put did not serialize as expected")
+
+      val deserializedPut = underTest.deserialize(serializedPut)
+      assert(orderedPut === deserializedPut,
+        "deserialize was not the inverse of serialize for Put")
+    }
+
+    it ("must properly serialize Deletes and be the inverse of serialize") {
+      val key = "hello"
+
+      val expected = ByteBuffer.allocate(8 + 8 + 4 + 4 + 4 + key.getBytes.length + 0)
+
+      expected.order(ByteOrder.BIG_ENDIAN)
+      expected.putLong(1234)
+      expected.putLong(5678)
+      expected.putInt(2)
+      expected.putInt(key.getBytes.length)
+      expected.putInt(0)
+      expected.put(key.getBytes)
+
+      val orderedDelete = OrderedEvent(1234, 5678, Delete(key))
+      val serializedDelete = underTest.serialize(orderedDelete)
+      assert(Arrays.equals(expected.array, serializedDelete),
+        "Delete did not serialize as expected")
+
+      val deserializedDelete = underTest.deserialize(serializedDelete)
+      assert(orderedDelete === deserializedDelete,
+        "deserialize was not the inverse of serialize for Delete")
+    }
+  }
+
+  describe("deserialize") {
+    it ("must properly deserialize Puts and be the inverse of serialize") {
+      val key = "hello"
+      val body = "world".getBytes
+
+      val expected = OrderedEvent(1234, 5678, Put(key, body))
+
+      val putBytesBuf = ByteBuffer.allocate(8 + 8 + 4 + 4 + 4 + key.getBytes.length + body.length)
+
+      putBytesBuf.order(ByteOrder.BIG_ENDIAN)
+      putBytesBuf.putLong(1234)
+      putBytesBuf.putLong(5678)
+      putBytesBuf.putInt(1)
+      putBytesBuf.putInt(key.getBytes.length)
+      putBytesBuf.putInt(body.length)
+      putBytesBuf.put(key.getBytes)
+      putBytesBuf.put(body)
+
+      val deserializedPut = underTest.deserialize(putBytesBuf.array)
+      assert(expected === deserializedPut,
+        "Put did not deserialize as expected")
+
+      val serializedPut = underTest.serialize(deserializedPut)
+      assert(Arrays.equals(putBytesBuf.array, serializedPut),
+        "serialize was not the inverse of deserialize for Put")
+    }
+
+    it ("must properly deserialize Deletes and be the inverse of deserialize") {
+      val key = "hello"
+
+      val expected = OrderedEvent(1234, 5678, Delete(key))
+
+      val putBytesBuf = ByteBuffer.allocate(8 + 8 + 4 + 4 + 4 + key.getBytes.length)
+
+      putBytesBuf.order(ByteOrder.BIG_ENDIAN)
+      putBytesBuf.putLong(1234)
+      putBytesBuf.putLong(5678)
+      putBytesBuf.putInt(2)
+      putBytesBuf.putInt(key.getBytes.length)
+      putBytesBuf.putInt(0)
+      putBytesBuf.put(key.getBytes)
+
+      val deserializedDelete = underTest.deserialize(putBytesBuf.array)
+      assert(expected === deserializedDelete,
+        "Delete did not deserialize as expected")
+
+      val serializedDelete = underTest.serialize(deserializedDelete)
+      assert(Arrays.equals(putBytesBuf.array, serializedDelete),
+        "serialize was not the inverse of deserialize for Delete")
+    }
+  }
+
+}
