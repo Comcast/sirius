dfc760a1e98182afcf8eaec921188ccaf555d691
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-08-27 11:46:07 -0400

    Make Base64Codec thread safe
    The alternative is to pass a fresh SerDe into the log requesting
    business things each time we spin them up, but we don't actually
    know the given impl due to the current construction.  This should
    be a good fix for now until we replace the log with the more
    efficient format.
    
    Change-Id: Ie1a85e7ec5a3f18e357ed29c304827e58ab8f2a0
    Reviewed-on: https://source.dev.cim.comcast.net/gerrit/4197
    Tested-by: GerritAHP
    Reviewed-by: michael bevilacqua-linn <michael_bevilacqua-linn@comcast.com>

--- src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/Base64PayloadCodec.scala
+++ src/main/scala/com/comcast/xfinity/sirius/writeaheadlog/Base64PayloadCodec.scala
@@ -5,12 +5,12 @@ import org.apache.commons.lang.StringUtils
 
 trait Base64PayloadCodec {
 
-  private[writeaheadlog] var payloadCodec = new Base64();
+  // XXX: needs to be a def, Base64 is not thread safe, but it is used in a non-thread-safe manner
+  private def payloadCodec = new Base64()
 
   def encodePayload(payload: Option[Array[Byte]]): String = payloadCodec.encodeToString(payload.getOrElse("".getBytes))
 
   def decodePayload(payload: String): Option[Array[Byte]] = {
-
     if (StringUtils.isEmpty(payload)) {
       None
     } else {
