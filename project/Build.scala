e3b7489f1cf12029b83f5f4562205ca8312b8ffd
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-09-11 15:36:37 -0400

    Build sirius-standalone using SBT
    No more long waits, build-interactive served this project great,
    but now we move on to bigger things.
    
    Change-Id: I33fbe6c58953d0c2817ae92a0fe03569ad940088
    Reviewed-on: https://source.dev.cim.comcast.net/gerrit/4749
    Tested-by: GerritAHP
    Reviewed-by: karl martino <karl_martino@comcast.com>

---
+++ project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+import java.lang.{Runtime => JRuntime}
+
+object SiriusTestHarnessBuild extends Build {
+
+  lazy val root = Project(
+    id = "sirius",
+    base = file("."),
+    settings = Defaults.defaultSettings ++ Seq(
+        makeStandalone <<= (packageBin in Compile, dependencyClasspath in Compile) map makeStandaloneFn))
+  
+  val makeStandalone = TaskKey[Unit]("make-standalone",
+    "Creates the sirius-standalone package")
+  
+  val packageDir = file("target/sirius-standalone")
+  val libDir = packageDir / "lib"
+  val binDir = packageDir / "bin"
+  
+  def makeStandaloneFn(packageBin: java.io.File, dependencies: Seq[Attributed[File]]) = { 
+    
+    // Move artifact and dependencies into lib directory in test package
+    val files = packageBin +: (for (dep <- dependencies) yield dep.data)
+    for(file <- files) {
+      IO.copyFile(file, libDir / file.getName)
+    }   
+
+    // Move startup scripts into bin directory in test package
+    IO.copyDirectory(file(".") / "src/main/bin", binDir)
+    (binDir ** "*.sh").getPaths.foreach(
+      (binFile) =>
+        JRuntime.getRuntime.exec("chmod 755 " + binFile)
+    )   
+    
+    JRuntime.getRuntime.exec("tar -C target -czf target/sirius-standalone.tgz sirius-standalone")
+  }
+  
+}
+
