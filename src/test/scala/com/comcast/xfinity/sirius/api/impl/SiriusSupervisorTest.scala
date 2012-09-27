1ac9099d8943bdedff7adbfc54c643ece3e1d25e
Michajlo Matijkiw <michajlo_matijkiw@comcast.com>
2012-09-27 11:56:24 -0400

    Slim down SiriusSupervisorTest
    In anticipation of making some changes in the supervisor. A lot of
    the complexity in the supervisor seems to stem from the desire to
    test it.
    
    Oh, it also takes 2 seconds off of the test time.
    
    Change-Id: I11bd3a0e05a420ce43b25afe4f25c73550ba559c
    Reviewed-on: https://source.dev.cim.comcast.net/gerrit/5258
    Tested-by: GerritAHP
    Reviewed-by: peter cline <peter_cline2@comcast.com>

--- src/test/scala/com/comcast/xfinity/sirius/api/impl/SiriusSupervisorTest.scala
+++ src/test/scala/com/comcast/xfinity/sirius/api/impl/SiriusSupervisorTest.scala
@@ -1,92 +1,42 @@
 package com.comcast.xfinity.sirius.api.impl
 
 import membership._
-import com.typesafe.config.ConfigFactory
-import com.comcast.xfinity.sirius.api.RequestHandler
-import com.comcast.xfinity.sirius.writeaheadlog.SiriusLog
 import akka.actor.{ActorRef, ActorSystem}
-import akka.testkit.{TestProbe, TestActor, TestActorRef}
-import akka.dispatch.Await
-import akka.pattern.ask
-import akka.util.duration._
-import akka.util.Timeout
+import akka.testkit.{TestProbe, TestActorRef}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import akka.agent.Agent
-import com.comcast.xfinity.sirius.api.SiriusResult
 import com.comcast.xfinity.sirius.{TimedTest, NiceTest}
+import org.scalatest.BeforeAndAfterAll
 
 
 @RunWith(classOf[JUnitRunner])
-class SiriusSupervisorTest extends NiceTest with TimedTest {
+class SiriusSupervisorTest extends NiceTest with BeforeAndAfterAll with TimedTest {
 
-  var actorSystem: ActorSystem = _
+  implicit val actorSystem = ActorSystem("SiriusSupervisorTest")
 
-  var paxosProbe: TestProbe = _
-  var persistenceProbe: TestProbe = _
-  var stateProbe: TestProbe = _
-  var membershipProbe: TestProbe = _
-
-  var handler: RequestHandler = _
-  var siriusLog: SiriusLog = _
+  val paxosProbe = TestProbe()
+  val persistenceProbe = TestProbe()
+  val stateProbe = TestProbe()
+  val membershipProbe = TestProbe()
 
   var supervisor: TestActorRef[SiriusSupervisor with SiriusSupervisor.DependencyProvider] = _
 
-  implicit val timeout: Timeout = (5 seconds)
+  override def afterAll() {
+    actorSystem.shutdown()
+  }
 
   before {
-    actorSystem = ActorSystem("testsystem", ConfigFactory.parseString("""
-    akka.event-handlers = ["akka.testkit.TestEventListener"]
-    """))
-
-    //setup mocks
-    handler = mock[RequestHandler]
-    siriusLog = mock[SiriusLog]
-
-    membershipProbe = TestProbe()(actorSystem)
-    membershipProbe.setAutoPilot(new TestActor.AutoPilot {
-      def run(sender: ActorRef, msg: Any): Option[TestActor.AutoPilot] = msg match {
-        case msg: MembershipMessage => Some(this)
-      }
-    })
-
-    paxosProbe = TestProbe()(actorSystem)
-    paxosProbe.setAutoPilot(new TestActor.AutoPilot {
-      def run(sender: ActorRef, msg: Any): Option[TestActor.AutoPilot] = msg match {
-        case Delete(_) =>
-          sender ! SiriusResult.some("Delete it")
-          Some(this)
-        case Put(_, _) =>
-          sender ! SiriusResult.some("Put it")
-          Some(this)
-      }
-    })
-
-    stateProbe = TestProbe()(actorSystem)
-    stateProbe.setAutoPilot(new TestActor.AutoPilot {
-      def run(sender: ActorRef, msg: Any): Option[TestActor.AutoPilot] = msg match {
-        case Get(_) =>
-          sender ! SiriusResult.some("Got it")
-          Some(this)
-      }
-    })
-
-    persistenceProbe = TestProbe()(actorSystem)
-
     supervisor = TestActorRef(new SiriusSupervisor with SiriusSupervisor.DependencyProvider {
-      val siriusStateAgent = Agent(new SiriusState)(context.system)
-      val membershipAgent = Agent(Set[ActorRef]())(context.system)
+      val siriusStateAgent: Agent[SiriusState] = Agent(new SiriusState)(context.system)
+      val membershipAgent: Agent[Set[ActorRef]] = Agent(Set[ActorRef]())(context.system)
 
       val stateSup: ActorRef = stateProbe.ref
       val membershipActor: ActorRef = membershipProbe.ref
       val orderingActor: ActorRef = paxosProbe.ref
-      val statusSubsystem = TestProbe()(context.system).ref
+      val statusSubsystem: ActorRef = TestProbe().ref
       
-    })(actorSystem)
-  }
-
-  after {
-    actorSystem.shutdown()
+    })
   }
 
   def initializeSupervisor(supervisor: TestActorRef[SiriusSupervisor with SiriusSupervisor.DependencyProvider]) {
@@ -95,9 +45,9 @@ class SiriusSupervisorTest extends NiceTest with TimedTest {
     // wait for agent to get updated, just in case
     assert(waitForTrue(siriusStateAgent().areSubsystemsInitialized, 1000, 250))
 
-    val isInitializedFuture = supervisor ? SiriusSupervisor.IsInitializedRequest
-    val expected = SiriusSupervisor.IsInitializedResponse(true)
-    assert(expected === Await.result(isInitializedFuture, timeout.duration))
+    val senderProbe = TestProbe()
+    senderProbe.send(supervisor, SiriusSupervisor.IsInitializedRequest)
+    senderProbe.expectMsg(SiriusSupervisor.IsInitializedResponse(true))
   }
 
   describe("a SiriusSupervisor") {
@@ -109,7 +59,7 @@ class SiriusSupervisorTest extends NiceTest with TimedTest {
     it("should transition into the initialized state") {
       initializeSupervisor(supervisor)
       val stateAgent = supervisor.underlyingActor.siriusStateAgent
-      assert(true === stateAgent.await(timeout).supervisorInitialized)
+      waitForTrue(stateAgent().supervisorInitialized, 5000, 250)
     }
 
     it("should forward MembershipMessages to the membershipActor") {
@@ -122,39 +72,22 @@ class SiriusSupervisorTest extends NiceTest with TimedTest {
     it("should forward GET messages to the stateActor") {
       initializeSupervisor(supervisor)
       val get = Get("1")
-      val getAskFuture = supervisor ? get
-      val expected = SiriusResult.some("Got it")
-      assert(expected === Await.result(getAskFuture, timeout.duration))
+      supervisor ! get
       stateProbe.expectMsg(get)
-      noMoreMsgs()
     }
     
     it("should forward DELETE messages to the paxosActor") {
       initializeSupervisor(supervisor)
       val delete = Delete("1")
-      val deleteAskFuture = supervisor ? delete
-      val expected = SiriusResult.some("Delete it")
-      assert(expected === Await.result(deleteAskFuture, timeout.duration))
+      supervisor ! delete
       paxosProbe.expectMsg(delete)
-      noMoreMsgs()
     }
 
     it("should forward PUT messages to the paxosActor") {
       initializeSupervisor(supervisor)
       val put = Put("1", "someBody".getBytes)
-      val putAskFuture = supervisor ? put
-      val expected = SiriusResult.some("Put it")
-      assert(expected === Await.result(putAskFuture, timeout.duration))
+      supervisor ! put
       paxosProbe.expectMsg(put)
-      noMoreMsgs()
     }
   }
-
-  def noMoreMsgs() {
-    membershipProbe.expectNoMsg((100 millis))
-    paxosProbe.expectNoMsg(100 millis)
-    persistenceProbe.expectNoMsg((100 millis))
-    stateProbe.expectNoMsg((100 millis))
-  }
-
 }
