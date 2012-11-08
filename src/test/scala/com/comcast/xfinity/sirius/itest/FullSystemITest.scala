ecdbd787b09421c225bc5caf839dd336e85188a8
Peter Cline <peter_cline2@comcast.com>
2012-11-08 10:02:06 -0500

    3rd try on making FullSystemITest consistent
    For master/slave mode.  This one keeps its hands out of the black box
    and instead sends canaries into the coal mine continuously until one
    comes back alive.
    
    Other changes:
    - reproposal window configurable in the itest via makeSirius
    - fireAndAwait now takes a list of commands instead of a start/end
      range.
    
    patchset2: rebased, fixed conflicts
    
    Change-Id: Ib3b8a04371e1989edee19d200c13447f50b54f84
    Reviewed-on: https://source.dev.cim.comcast.net/gerrit/6423
    Tested-by: GerritAHP
    Reviewed-by: michajlo matijkiw <michajlo_matijkiw@comcast.com>

--- src/test/scala/com/comcast/xfinity/sirius/itest/FullSystemITest.scala
+++ src/test/scala/com/comcast/xfinity/sirius/itest/FullSystemITest.scala
@@ -59,7 +59,8 @@ class FullSystemITest extends NiceTest with TimedTest {
                  handler: Option[RequestHandler] = None,
                  wal: Option[SiriusLog] = None,
                  chunkSize: Int = 100,
-                 gapRequestFreqSecs: Int = 30):
+                 gapRequestFreqSecs: Int = 30,
+                 replicaReproposalWindow: Int = 10):
                 (SiriusImpl, RequestHandler, SiriusLog) = {
 
     val finalHandler = handler match {
@@ -83,6 +84,7 @@ class FullSystemITest extends NiceTest with TimedTest {
     siriusConfig.setProp(SiriusConfiguration.LOG_REQUEST_CHUNK_SIZE, chunkSize)
     siriusConfig.setProp(SiriusConfiguration.LOG_REQUEST_FREQ_SECS, gapRequestFreqSecs)
     siriusConfig.setProp(SiriusConfiguration.PAXOS_MEMBERSHIP_CHECK_INTERVAL, 0.1)
+    siriusConfig.setProp(SiriusConfiguration.REPROPOSAL_WINDOW, replicaReproposalWindow)
 
     val sirius = SiriusFactory.createInstance(
       finalHandler,
@@ -132,7 +134,7 @@ class FullSystemITest extends NiceTest with TimedTest {
     Thread.sleep(2000)
   }
 
-  def fireAndAwait(sirii: List[SiriusImpl], commands: List[String]) = {
+  def fireAndAwait(sirii: List[SiriusImpl], commands: List[String]): List[String] = {
     val numSirii = sirii.size
     val futuresAndCommands = commands.map(
       (i) => {
@@ -154,18 +156,19 @@ class FullSystemITest extends NiceTest with TimedTest {
     failedFuturesAndCommands.map(_._2)
   }
 
-  def fireAndRetryCommands(sirii: List[SiriusImpl], first: Int, last: Int, retries: Int) = {
-    @tailrec
-    def fireAndRetryCommandsAux(sirii: List[SiriusImpl], commands: List[String], retries: Int): List[String] = {
-      val failedCommands = fireAndAwait(sirii, commands)
-      println("*** %s retries left, need to retry %s commands".format(retries, failedCommands.size))
-      if (retries > 0 && !failedCommands.isEmpty) {
-        fireAndRetryCommandsAux(sirii, failedCommands, retries - 1)
-      } else {
-        failedCommands
-      }
+  @tailrec
+  final def fireAndRetryCommands(sirii: List[SiriusImpl], commands: List[String], retries: Int): List[String] = {
+    val failedCommands = fireAndAwait(sirii, commands)
+    println("*** %s retries left, need to retry %s commands".format(retries, failedCommands.size))
+    if (retries > 0 && !failedCommands.isEmpty) {
+      fireAndRetryCommands(sirii, failedCommands, retries - 1)
+    } else {
+      failedCommands
     }
-    fireAndRetryCommandsAux(sirii, List.range(first, last + 1).map(_.toString), retries)
+  }
+
+  def generateCommands(first: Int, last: Int): List[String] = {
+    List.range(first, last + 1).map(_.toString)
   }
 
   describe("a full sirius implementation") {
@@ -177,7 +180,7 @@ class FullSystemITest extends NiceTest with TimedTest {
       sirii = List(sirius1, sirius2, sirius3)
       waitForMembership(sirii, 3)
 
-      val failed = fireAndRetryCommands(sirii, 1, numCommands, 4)
+      val failed = fireAndRetryCommands(sirii, generateCommands(1, numCommands), 4)
       println("No response for %s out of %s".format(failed.size, numCommands))
       assert(0 === failed.size, "There were failed commands")
 
@@ -199,7 +202,7 @@ class FullSystemITest extends NiceTest with TimedTest {
       sirii = List(sirius1, sirius2)
       waitForMembership(sirii, 3)
 
-      val failed = fireAndRetryCommands(sirii, 1, numCommands, 4)
+      val failed = fireAndRetryCommands(sirii, generateCommands(1, numCommands), 4)
       println("No response for %s out of %s".format(failed.size, numCommands))
 
       assert(waitForTrue(verifyWalSize(log1, numCommands), 30000, 500),
@@ -230,13 +233,13 @@ class FullSystemITest extends NiceTest with TimedTest {
       )
 
       val numCommands = 50
-      val (sirius1, _, log1) = makeSirius(42289)
-      val (sirius2, _, log2) = makeSirius(42290, gapRequestFreqSecs = 5)
-      val (sirius3, _, log3) = makeSirius(42291)
+      val (sirius1, _, log1) = makeSirius(42289, replicaReproposalWindow = 4)
+      val (sirius2, _, log2) = makeSirius(42290, replicaReproposalWindow = 4, gapRequestFreqSecs = 5)
+      val (sirius3, _, log3) = makeSirius(42291, replicaReproposalWindow = 4, gapRequestFreqSecs = 5)
       sirii = List(sirius1, sirius2, sirius3)
       waitForMembership(sirii, 3)
 
-      val failed = fireAndRetryCommands(sirii, 1, numCommands, 4)
+      val failed = fireAndRetryCommands(sirii, generateCommands(1, numCommands), 4)
       println("No response for %s out of %s".format(failed.size, numCommands))
 
       assert(waitForTrue(verifyWalSize(log1, numCommands), 30000, 500),
@@ -249,30 +252,44 @@ class FullSystemITest extends NiceTest with TimedTest {
       assert(waitForTrue(verifyWalsAreEquivalent(List(log1, log2, log3)), 500, 100),
         "Wals were not equivalent")
 
-      // ok, now let's take 2 out of the cluster...
-      // needs to be 1 or 2, since 3 is (probably) the leader and we don't want to sit here
-      //   and wait for leader election.
-      // XXX "probably" above, arg
-
       path.truncate(0)
       path.append(
         "akka://sirius-42289@localhost:42289/user/sirius\n" +
-        "akka://sirius-42291@localhost:42291/user/sirius\n"
+        "akka://sirius-42290@localhost:42290/user/sirius\n"
       )
       waitForMembership(sirii, 2)
 
-      val failed2 = fireAndRetryCommands(List(sirius1, sirius3), numCommands + 1, numCommands * 2, 4)
+      val nextCommandSet = generateCommands(numCommands + 1, numCommands * 2)
+      val nextCommand = nextCommandSet.head
+      val nextCommandTail = nextCommandSet.tail
+
+      // use the first command to test... keep going until it finishes.
+      // 30 seconds here basically equates to 5 tries (5 sec ask timeout
+      // and 1 sec between). this is basically waiting for leader election
+      // to occur (if it's necessary).
+      var retried = 0
+      assert(waitForTrue({
+        val failed = fireAndAwait(List(sirius1), List(nextCommand))
+        if (!failed.isEmpty) {
+          retried += 1
+          println("XXX %s retries so far", retried)
+        }
+        failed.isEmpty
+      }, 30000, 1000), "after removing Sirius3 from the cluster, could not get any commands through the system")
+
+      // then fire off the rest
+      val failed2 = fireAndRetryCommands(List(sirius1, sirius2), nextCommandTail, 4)
       println("No response for %s out of %s".format(failed2.size, numCommands))
 
-      // nodes of log1 and log3 are running normally
+      // nodes of log1 and log2 are running normally
       assert(waitForTrue(verifyWalSize(log1, numCommands * 2), 30000, 500),
         "Wal 1 did not contain expected number of events (%s out of %s)".format(getWalSize(log1), numCommands * 2))
-      assert(waitForTrue(verifyWalSize(log3, numCommands * 2), 30000, 500),
-        "Wal 3 did not contain expected number of events (%s out of %s)".format(getWalSize(log3), numCommands * 2))
-
-      // log2 is in slave mode, catching up
       assert(waitForTrue(verifyWalSize(log2, numCommands * 2), 30000, 500),
-        "Slave did not catch up for all commands (%s out of %s)".format(getWalSize(log2), numCommands * 2))
+        "Wal 2 did not contain expected number of events (%s out of %s)".format(getWalSize(log2), numCommands * 2))
+
+      // log3 is in slave mode, catching up
+      assert(waitForTrue(verifyWalSize(log3, numCommands * 2), 30000, 500),
+        "Slave did not catch up for all commands (%s out of %s)".format(getWalSize(log3), numCommands * 2))
 
       assert(waitForTrue(verifyWalsAreEquivalent(List(log1, log2, log3)), 500, 100),
         "Master and slave wals not equivalent")
