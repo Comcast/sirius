f3d1b3e8cbd708c201108b9ad44188937fdce508
Jonathan Ryan <jonathan_ryan@comcast.com>
2020-01-23 16:38:24 -0500

    Add encrypted gpg file

---
+++ .travis/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+tagVer=${TRAVIS_TAG}
+tagVer=${tagVer#v}   # Remove `v` at beginning.
+tagVer=${tagVer%%#*} # Remove anything after `#`.
+publishVersion='set every version := "'$tagVer'"'
+
+java -version
+echo "Releasing $tagVer with Scala $TRAVIS_SCALA_VERSION"
+
+echo "$PGP_PASSPHRASE" | gpg --passphrase-fd 0 --batch --yes --import .travis/secret-key.asc
+
+sbt "$publishVersion" "+publishSigned"
+
+
