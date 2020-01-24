90a9c2b9af964be47236f9eec128f6ba047113be
Jonathan Ryan <jonathan_ryan@comcast.com>
2020-01-24 12:15:06 -0500

    Test...

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -6,7 +6,6 @@ tagVer=${tagVer%%#*} # Remove anything after `#`.
 publishVersion='set every version := "'$tagVer'"'
 
 java -version
-echo "Releasing $tagVer with Scala $TRAVIS_SCALA_VERSION"
 
 echo "$PGP_PASSPHRASE" | gpg --passphrase-fd 0 --batch --yes --import .travis/secret-key.asc
 
