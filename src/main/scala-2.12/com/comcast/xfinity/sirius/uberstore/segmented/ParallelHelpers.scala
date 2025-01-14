611366bc96d7d4ccb993e76db43c79ec545ddf0a
Spindler, Justin <Justin_Spindler@comcast.com>
2025-01-14 13:37:06 -0500

    Change package

--- src/main/scala-2.12/com/comcast/xfinity/sirius/uberstore/segmented/ParallelHelpers.scala
+++ src/main/scala-2.12/com/comcast/xfinity/sirius/uberstore/segmented/ParallelHelpers.scala
@@ -1,6 +1,6 @@
 package com.comcast.xfinity.sirius.uberstore.segmented
 
-import scala.collection.parallel.immutable.ParSeq
+import scala.collection.parallel.ParSeq
 
 object ParallelHelpers {
     def parallelize[T](seq: Seq[T]): ParSeq[T] = seq.par
--- src/main/scala-2.13/com/comcast/xfinity/sirius/uberstore/segmented/ParallelHelpers.scala
+++ src/main/scala-2.13/com/comcast/xfinity/sirius/uberstore/segmented/ParallelHelpers.scala
@@ -1,7 +1,7 @@
 package com.comcast.xfinity.sirius.uberstore.segmented
 
-import scala.collection.parallel.immutable.ParSeq
 import scala.collection.parallel.CollectionConverters._
+import scala.collection.parallel.ParSeq
 
 object ParallelHelpers {
     def parallelize[T](seq: Seq[T]): ParSeq[T] = seq.par
