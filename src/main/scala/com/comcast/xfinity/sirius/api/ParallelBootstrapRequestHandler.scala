604df1fe85df2e94ec2049769afa2d0c06139971
Spindler, Justin <Justin_Spindler@comcast.com>
2025-01-16 16:43:24 -0500

    Implementation of RequestHandler to support parallel out-of-order bootstrapping

---
+++ src/main/scala/com/comcast/xfinity/sirius/api/ParallelBootstrapRequestHandler.scala
@@ -0,0 +1,46 @@
+package com.comcast.xfinity.sirius.api
+
+import java.util.concurrent.ConcurrentHashMap
+
+class ParallelBootstrapRequestHandler(val requestHandler: RequestHandler) extends RequestHandler {
+    private var sequences = new ConcurrentHashMap[String, Long]
+
+    override def onBootstrapStarting(): Unit = requestHandler.onBootstrapStarting()
+    override def handleGet(key: String): SiriusResult = requestHandler.handleGet(key)
+    override def handlePut(key: String, body: Array[Byte]): SiriusResult = requestHandler.handlePut(key, body)
+    override def handleDelete(key: String): SiriusResult = requestHandler.handleDelete(key)
+
+    override def onBootstrapComplete(): Unit = {
+        // allow the GC to collect the map
+        sequences = null
+        requestHandler.onBootstrapComplete()
+    }
+
+    override def handlePut(sequence: Long, key: String, body: Array[Byte]): SiriusResult = {
+        latest(key, sequence) {
+            requestHandler.handlePut(sequence, key, body)
+        }
+    }
+
+    override def handleDelete(sequence: Long, key: String): SiriusResult = {
+        latest(key, sequence) {
+            requestHandler.handleDelete(sequence, key)
+        }
+    }
+
+    private def latest(key: String, sequence: Long)(f: => SiriusResult) = {
+        val sequences = this.sequences
+        if (sequences != null) {
+            var result = SiriusResult.none()
+            sequences.compute(key, (_, existing) => {
+                if (existing < sequence) {
+                    result = f
+                    sequence
+                } else existing
+            })
+            result
+        } else {
+            f
+        }
+    }
+}
